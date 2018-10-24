428e19ade90f4996a149756c555204b6508fca25
Matt Addison <matthew.addison@talk21.com>
2018-10-24 21:58:06 +0100

    finished 3 from 5 mvp tasks

--- src/main/java/com/codeclan/example/WhiskyTracker/repositories/WhiskyRepository/WhiskyRepositoryImpl.java
+++ src/main/java/com/codeclan/example/WhiskyTracker/repositories/WhiskyRepository/WhiskyRepositoryImpl.java
@@ -1,7 +1,12 @@
 package com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository;
 
 
+import com.codeclan.example.WhiskyTracker.models.Distillery;
 import com.codeclan.example.WhiskyTracker.models.Whisky;
+import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,13 +16,48 @@ import java.util.List;
 public class WhiskyRepositoryImpl implements WhiskyRepositoryCustom {
 
 
-//    TODO: get all whiskeys for a particular year
+//    TODO: get all whiskys for a particular year
 
     @Autowired
     EntityManager entityManager;
 
     @Transactional
     public List<Whisky> findAllWhiskeysForYear(int year){
+        List<Whisky> results = null;
 
+        Session session = entityManager.unwrap(Session.class);
+        try {
+            Criteria cr  = session.createCriteria(Whisky.class);
+            cr.add(Restrictions.eq("year", year));
+            results = cr.list();
+        } catch(HibernateException ex) {
+            ex.printStackTrace();
+        } finally {
+            session.close();
+        }
+        return results;
     }
+
+//    TODO: get all whiskies from a particular region
+
+    @Transactional
+    public List<Whisky> findAllWhiskeysFromRegion(String region){
+        List<Whisky> results = null;
+
+        Session session = entityManager.unwrap(Session.class);
+        try{
+            Criteria cr = session.createCriteria(Distillery.class);
+            cr.add(Restrictions.eq("region", region));
+            results = cr.list();
+        } catch(HibernateException ex) {
+            ex.printStackTrace();
+        } finally {
+            session.close();
+        }
+        return results;
+    }
+
+//    TODO: get all the whisky from a particular distillery that's a specific age (if any)
+
+
 }
